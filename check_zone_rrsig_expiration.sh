69ea806e8a28dd36c0c52b70bb7742640a6a3676
lhalbert <lucashalbert@users.noreply.github.com>
2019-03-19 09:47:01 -0400

    Add contact email

--- check_zone_rrsig_expiration.sh
+++ check_zone_rrsig_expiration.sh
@@ -3,7 +3,7 @@ print_version() {
     cat <<EOF
 ####################################################################################
 #
-# Author:       Lucas Halbert <https://www.lhalbert.xyz>
+# Author:       Lucas Halbert <contactme@lhalbert.xyz>
 # Date:         03/17/2017
 # Last Edited:  02/28/2019
 # Version:      2019.02.28
