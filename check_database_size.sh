97060dad598a4a108beecfa5d15dbf0835706a92
lhalbert <lucashalbert@users.noreply.github.com>
2019-03-19 09:45:19 -0400

    Add contact email

--- check_database_size.sh
+++ check_database_size.sh
@@ -3,7 +3,7 @@ print_version() {
     cat <<EOF
 ####################################################################################
 #
-# Author:       Lucas Halbert <https://www.lhalbert.xyz>
+# Author:       Lucas Halbert <contactme@lhalbert.xyz>
 # Date:         12/01/2015
 # Last Edited:  02/28/2019
 # Version:      2019.28.02
